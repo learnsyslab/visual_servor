ab52c7bbad0c88d7e50d9acf39703e561f504e42
Adam Heins <mail@adamheins.com>
2025-06-25 11:31:17 -0400

    WIP: refine the pendulum simulation.

--- scripts/simulation/pendulum.py
+++ scripts/simulation/pendulum.py
@@ -21,12 +21,8 @@ GRAVITY = np.array([0, 0, -9.81])
 LENGTH = 0.3
 DAMPING = 0
 
-ALLOW_ROTATION = True
-USE_FRICTION_CONSTRAINTS = True
-USE_TENSILE_FORCE_CONSTRAINT = True
-
 # height of the object's CoM above its base
-OBJ_COM_HEIGHT = 0.05
+OBJ_COM_HEIGHT = 0.0
 OBJ_BASE_HALF_LENGTH = 0.1
 
 TRAY_RADIUS = 0.2
@@ -51,30 +47,29 @@ def unit(v):
     return v / norm
 
 
+def contact_jac(point):
+    return np.vstack((rg.skew3(point), np.eye(3)))
+
+
 # inertial parameters
 com_p = np.array([0, 0, -LENGTH])
 com_o = com_p + np.array([0, 0, OBJ_COM_HEIGHT])
 
+# NOTE: inertias are really important for all results!
 params_p = rg.InertialParameters(
     mass=1,
     com=com_p,
-    I=2 * np.diag([0.01, 0.02, 0.03]),
+    I=np.diag([0.001, 0.001, 0.001]),
     translate_from_com=True,
 )
 params_o = rg.InertialParameters(
-    mass=1, com=com_o, I=0.01 * np.eye(3), translate_from_com=True
+    mass=1, com=com_o, I=0.0 * np.eye(3), translate_from_com=True
 )
 params_po = params_p + params_o
 
 # spatial mass matrices
 M_o = params_o.M
 M_p = params_p.M
-M_po = params_po.M
-
-
-def contact_jac(point):
-    return np.vstack((rg.skew3(point), np.eye(3)))
-
 
 # contact points
 contacts = np.array(
@@ -88,7 +83,7 @@ contacts = np.array(
 Gs = [contact_jac(c) for c in contacts]
 
 mG_o = params_o.mass * contact_jac(params_o.com)
-mG_po = params_po.mass * contact_jac(params_po.com)
+mG_p = params_p.mass * contact_jac(params_p.com)
 
 # anchor points for tray ropes
 anchors = np.array(
@@ -102,54 +97,47 @@ anchors = np.array(
 
 
 # friction matrix: F @ f <= 0 means f inside friction cone
-F = np.array(
-    [[0, 0, -1], [1, 1, -MU], [1, -1, -MU], [-1, -1, -MU], [-1, 1, -MU]]
-)
+F = np.array([[0, 0, -1], [1, 1, -MU], [1, -1, -MU], [-1, -1, -MU], [-1, 1, -MU]])
 
 # initial state
 ξ = rg.SV.zero()
 C = np.eye(3)  # this is C_wb
 
-
 # spatial acceleration
 # we can compute the linear part analytically
-ωdot = cp.Variable(3)
+ωdot = cp.Parameter(3)
 vdot = cp.Parameter(3)
 ξdot = cp.hstack([ωdot, vdot])
 
 # remainder wrenches
-rem_po = cp.Parameter(6)
+rem_p = cp.Parameter(6)
 rem_o = cp.Parameter(6)
 
 # contact forces
 fs = cp.Variable((4, 3))
-z = fs.flatten()
 
-# tensile force along c
+# total contact wrench on the object
+# this is applied the opposite way on the tray
+wc = cp.sum([G @ f for G, f in zip(Gs, fs)])
+
+# tensile force along ropes
+# no torque because forces all act through the origin
 ft = cp.Variable(3)
 wt = cp.hstack((np.zeros(3), ft))
 
-# total contact wrench on the object
-wc = cp.sum([G @ f for G, f in zip(Gs, fs)])
+# minimize the shear contact forces
+W = np.diag([1, 1, 0])
+objective = cp.Minimize(cp.sum([cp.quad_form(f, W, assume_PSD=True) for f in fs]))
 
-# minimize the contact forces
-objective = cp.Minimize(cp.quad_form(z, np.eye(12), assume_PSD=True))
+# Newton-Euler equations (force-torque balance)
+constraints = [rem_o + wc == M_o @ ξdot, rem_p - wc + wt == M_p @ ξdot]
 
-constraints = [rem_o + wc == M_o @ ξdot]
+# friction constraints
+constraints.extend([F @ f <= 0 for f in fs])
 
-if USE_FRICTION_CONSTRAINTS:
-    constraints.extend([F @ f <= 0 for f in fs])
-
-if USE_TENSILE_FORCE_CONSTRAINT:
-    # also enforce that tensile forces must be positive
-    # constraints.append(ft @ params_po.com <= 0)
-    fts = cp.Variable(4, nonneg=True)
-    constraints.append(ft == cp.sum([-f * unit(a) for f, a in zip(fts, anchors)]))
-
-if ALLOW_ROTATION:
-    constraints.append(rem_po + wt == M_po @ ξdot)
-else:
-    constraints.append(ωdot == 0)
+# tensile forces must be positive
+fts = cp.Variable(4, nonneg=True)
+constraints.append(ft == cp.sum([-f * unit(a) for f, a in zip(fts, anchors)]))
 
 problem = cp.Problem(objective, constraints)
 
@@ -157,33 +145,24 @@ ts = []
 us = []
 φs = []
 ξs = []
+ffs = []  # TODO name
 
-# TODO why is there z velocity?
 t = 0
 for i in range(STEPS):
     t = i * TIMESTEP
     u = input_accel(t)
 
-    # manually damp out the system
-    # if t > 2:
-    #     u = C @ (1 * np.cross(ξ.angular, params_p.com) - 1 * ξ.linear + np.cross(ξ.angular, ξ.linear))
-
-    # inject energy into the system and then stabilize velocity
-    if 2 < t < 4:
-        # TODO it is unlikely we can actually pull this off with only a tensile force
-        vpx = (C @ np.cross(ξ.angular, params_p.com))[0]
-        u = np.array([-vpx, 0, 0])
-    elif t >= 4:
-        u = -C @ ξ.linear
-
+    # solve for spatial acceleration
+    ωdot.value = np.linalg.solve(
+        params_po.I,
+        np.cross(params_po.h, C.T @ (GRAVITY - u))
+        - np.cross(ξ.angular, params_po.I @ ξ.angular),
+    )
     vdot.value = C.T @ u - np.cross(ξ.angular, ξ.linear)
 
-    wg_po = mG_po @ C.T @ GRAVITY
-    rem_po.value = ξ.adjoint().T @ M_po @ ξ.vec + wg_po
-    # + np.concatenate((-DAMPING * ξ.angular, np.zeros(3)))
-
-    wg_o = mG_o @ C.T @ GRAVITY
-    rem_o.value = ξ.adjoint().T @ M_o @ ξ.vec + wg_o
+    # gravity and Coriolis forces on each body
+    rem_p.value = ξ.adjoint().T @ M_p @ ξ.vec + mG_p @ C.T @ GRAVITY
+    rem_o.value = ξ.adjoint().T @ M_o @ ξ.vec + mG_o @ C.T @ GRAVITY
 
     problem.solve(solver=cp.CLARABEL)
     if problem.status != "optimal":
@@ -191,6 +170,7 @@ for i in range(STEPS):
         IPython.embed()
         break
 
+    # integrate forward in time
     ξ = ξ + rg.SV(linear=vdot.value, angular=ωdot.value) * TIMESTEP
     C = C @ expm(rg.skew3(ξ.angular) * TIMESTEP)
 
@@ -201,9 +181,12 @@ for i in range(STEPS):
     # rotate into the world frame
     ξs.append(np.concatenate((C @ ξ.angular, C @ ξ.linear)))
 
+    ffs.append(fs.value)
+
 us = np.array(us)
 φs = np.array(φs)
 ξs = np.array(ξs)
+ffs = np.array(ffs)
 
 # φs[φs < 0] += 2 * np.pi
 
@@ -245,6 +228,16 @@ plt.plot(ts, ξs[:, 2], label="z")
 plt.grid()
 plt.xlabel("Time [s]")
 plt.ylabel("Angular velocity [rad/s]")
+plt.legend()
+
+plt.figure()
+plt.title("Contact forces")
+plt.plot(ts, ffs[:, 0, 0], label="x")
+plt.plot(ts, ffs[:, 0, 1], label="y")
+plt.plot(ts, ffs[:, 0, 2], label="z")
+plt.grid()
+plt.xlabel("Time [s]")
+plt.ylabel("Contact force [N]")
 plt.legend()
 
 plt.show()
